0a619cde87d9a8017d912975036fec99c8c0b8e5
abbey-sys <117507939+abbey-sys@users.noreply.github.com>
2024-02-23 12:26:19 +0800

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -61,12 +61,12 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-# ... place you code here to LIST accounts ...
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
-    List all Accounts
-    This endpoint will list all Accounts
+        List all Accounts
+        This endpoint will list all Accounts
     """
     app.logger.info("Request to list Accounts")
 
@@ -76,11 +76,12 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
+
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -95,11 +96,12 @@ def get_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
-######################################################################
-# UPDATE AN EXISTING ACCOUNT
+
+#####################################################################
+    # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -117,11 +119,12 @@ def update_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
@@ -135,6 +138,7 @@ def delete_accounts(account_id):
         account.delete()
 
     return "", status.HTTP_204_NO_CONTENT
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
